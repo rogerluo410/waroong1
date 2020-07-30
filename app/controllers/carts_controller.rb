7fe937d9716ff7db4e69f1ccb1a163f41c887599
rogerluo410 <rogerluo410@gmail.com>
2020-07-30 13:54:12 +0800

    Update ui

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,7 +1,7 @@
 class CartsController < ApplicationController
   def show
     add_breadcrumb '购物车'
-    @addresses = current_user.addresses.where("is_delete is false")
+    @addresses = current_user.addresses.where("is_delete is false") if current_user.present?
     @order_items = current_order.order_items.includes(product: [image_attachment: :blob])
   end
 end
